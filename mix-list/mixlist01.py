907cb79d3058197e3c940ae9c841129f9286bb57
twilliams2022 <tyreik.mm.williams@gmail.com>
2023-01-10 18:03:08 +0000

    list, input, printing variables

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -14,3 +14,28 @@ iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
 print("only ip addresses " + iplist[3])
 
 print("2nd ip address " + iplist[4])
+
+
+
+# exercise 31
+import random
+def main():
+wordbank= ["indentation", "spaces"]
+wordbank.append(4)
+num= int( input("Pick a student number between 1 and {len(tlgstudents)} !"))-1
+name= tlgstudent[num]
+print("You picked: " + num)
+print(wordbank)
+
+
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+print(tlgstudents)
+
+  name2 = random.choice(tlgstudents)
+    print(f"{name2}")
+
+
+
+main()
+
